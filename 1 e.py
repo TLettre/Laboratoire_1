53c7b7c5af0f46543e245230003d40f9ba9d7cdf
Tristan Lettre <206296182@cegepsherbrooke.qc.ca>
2025-01-22 13:35:39 -0500

    exercice1
    exercice

---
+++ 1 e.py
@@ -0,0 +1,14 @@
+print( )
+print("Bonjour")
+
+while True:
+
+    x=int(input()) 
+    y=int(input())
+    
+    print("=")
+    somme=x+y
+
+    print(somme)
+    print( )
+    
---
+++ 2 e.py
@@ -0,0 +1,13 @@
+print("bonjour")
+
+import math
+
+while True:
+    Number= int(input())
+
+    Number=math.factorial(Number)
+
+    print("=")
+    print(Number)
+    print( )
+
