e43b0502ff1d28df50f6b05a92be4715b298fffb
Tristan Lettre <206296182@cegepsherbrooke.qc.ca>
2025-01-27 11:47:59 -0500

    update
    bug fix and new upgrade

--- interface.py
+++ interface.py
@@ -1,3 +1,4 @@
+
 import tkinter as tk
 from tkinter import ttk
 import time
@@ -10,36 +11,37 @@ price1=100
 price2=10000
 
 oflineprice1=100
+oflineprice2=1000
 oflineclick=0
 
 
 
 while True:
 
-
+    ## loop
     def update_label():
         global click
         click = click + oflineclick
-        texte.config(text=f"click {click}")
+        texte.config(text=f"click: {click}")
         game.after(1000,update_label)
 
 
- 
+    ## click
     def onclick():
         global tap
         global texte
         global click
         click = click + tap 
         click = int(click)
-        texte.config(text=f"click {click}")
+        texte.config(text=f"click: {click}")
 
-
+    ## upgrade
     def upgrade1():
-        global tap
         global click
         global price1
-        global price3
         if click >= price1:
+            global tap
+            global price3
             tap = tap + 1
             click=click-price1
             price1=price1 * 1.1
@@ -79,12 +81,14 @@ while True:
             tap = tap * 1.05
             click=click-price3
             price3=tap*500
+            price3= int(price3)
             click= int(click)
             tap= int(tap)
             texte.config(text=f"click: {click}")
             taptexte.config(text=f"tap: {tap}")
             button4.config(text=f"{price3} = *1.05")
 
+    ## Afk upgrade
     def upgrade4():
         global click
         global oflineclick
@@ -96,26 +100,53 @@ while True:
             oflineprice1= int(oflineprice1)
             oflinebutton1.config(text=f"{oflineprice1} = +1s")
             texte.config(text=f"click: {click}")
-            print(oflineclick)
+            afkgain.config(text=f"afk tap: {oflineclick}s")
             if oflineclick == 1:
                 update_label()
+            if oflineclick == 25:
+                oflinebutton2.config(text=f"{oflineprice2} = +10s")
 
+
+    def upgrade5():
+        global click
+        global oflineprice2
+        global oflineclick
+        if oflineclick >= 25:
+            if click >= oflineprice2:
+                oflineclick=oflineclick + 10
+                click=click-oflineprice2
+                oflineprice2=oflineprice2*1.05
+                oflineprice2=int(oflineprice2)
+                oflinebutton2.config(text=f"{oflineprice2} = +10s")
+                texte.config(text=f"click: {click}")
+                afkgain.config(text=f"afk tap: {oflineclick}s")
+
+            
+
+    ## Interface
     
     texte = tk.Label(game, text=(f" click: {click}"))
-    texte2 = tk.Label(game, text=("ofline gain"))
+    texte2 = tk.Label(game, text=("afk gain"))
     taptexte= tk.Label(game, text=(f"tap: {tap}"))
+    afkgain= tk.Label(game, text=(f"afk tap: {oflineclick}s"))
+    upgade= tk.Label(game, text=("upgrade"))
 
     button1 = tk.Button(game, text="Cliquez-moi", command = onclick)
     button2 = tk.Button(game, text=f"{price1} = +1", command=upgrade1 )
     button3 = tk.Button(game, text=f"{price2} = +10", command= upgrade2)
     button4 = tk.Button(game, text=" 200 tap needed = *1.02", command=upgrade3)
     oflinebutton1= tk.Button(game, text=f"{oflineprice1} = +1s",command=upgrade4 )
-
+    oflinebutton2= tk.Button(game, text="25 afk tap needed = +10s",command=upgrade5)
+    ## Visual
 
     texte.pack(pady=5)
     taptexte.pack(pady=5)
+    afkgain.pack(pady=5)
 
-    button1.pack(pady=5)
+    button1.pack(pady=15)
+
+    upgade.pack(pady=5)
+
     button2.pack(pady=5)
     button3.pack(pady=5)
     button4.pack(pady=5)
@@ -123,8 +154,9 @@ while True:
     texte2.pack(pady=5)
 
     oflinebutton1.pack(pady=5)
+    oflinebutton2.pack(pady=5)
 
 
-  
+    
     game.mainloop()
 
