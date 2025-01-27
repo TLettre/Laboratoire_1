ff889c093af140665ed7bbb5a1a045c5afa66328
Tristan Lettre <206296182@cegepsherbrooke.qc.ca>
2025-01-27 12:19:30 -0500

    start
    start

---
+++ CookieClicker.py
@@ -0,0 +1,14 @@
+#game
+import tkinter as tk
+from tkinter import ttk
+import time
+
+root = tk.Tk()
+root.geometry("200x200")
+root.title("Idle game")
+
+windowsizeX=200
+windowsizeY=200
+root.minsize(windowsizeX, windowsizeY)
+root.maxsize(windowsizeX, windowsizeY)
+root.mainloop()
